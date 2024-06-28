af44159829997573576cd8e0ea146faa81c4dfd6
dmo575 <dmo5@protonmail.com>
2024-06-27 20:38:41 -0400

    reverting back ref idea

--- server.ts
+++ server.ts
@@ -136,7 +136,7 @@ async function handle_http(req: Request) {
 
     // record visit (time and resource requested)
     if(format == "html" || format == "md" || format == "pdf") {
-        record_visit(url.toJSON(), format, url.searchParams.get("ref"));
+        record_visit(url.toJSON(), format);
     }
 
     // try to get the file
@@ -153,7 +153,7 @@ async function handle_http(req: Request) {
 }
 
 // This just saves what pages the clients visit
-async function record_visit(requested_path: string, format: string, ref: string | null) {
+async function record_visit(requested_path: string, format: string) {
 
     // I blacklisted some URLs to avoid cluttering the records.log file
     if(!whitelist_Url(requested_path)) return;
@@ -175,15 +175,12 @@ async function record_visit(requested_path: string, format: string, ref: string 
     // resource format
     const format_str = get_data_str("type", format.toUpperCase(), 9);
 
-    // from where the link comes
-    const ref_str = get_data_str("ref", ref?.toUpperCase() || "???", 5);
-
     // the URL
     const url_str = "URL=" + requested_path;
 
     // encode record to Uint8Array which is what Deno.writeFile accepts.
     const encoder = new TextEncoder();
-    const record_encoded = encoder.encode(date_str + " " + format_str + " " + ref_str + " " + url_str + "\n");
+    const record_encoded = encoder.encode(date_str + " " + format_str + " " + url_str + "\n");
 
     // record it
     await Deno.writeFile(records, record_encoded, { append: true });
@@ -234,10 +231,6 @@ function parse_month(date:number): string {
 
 // choppy whitelist function to reduce cluster in records.log
 function whitelist_Url(url: string): boolean {
-
-    // we first sort the links that contain the ref query parameter. Since we use that param in our links, we can allow them all.
-    if(url.includes("?ref=") && url.includes("alfredcode.com"))
-        return true;
 
     // filters out requests not using the domain name (most bots)
     if(!url.includes("alfredcode.com"))
